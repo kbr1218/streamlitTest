fc672a19cbada2c88e5802e9ea519c89ad502b2f
kbr_1218 <kbr_uni@naver.com>
2024-10-11 17:46:07 +0900

    [ADD] 월별 카테고리별 비중 시각화 그래프 추가

--- pages/subpages/tab_trend.py
+++ pages/subpages/tab_trend.py
@@ -4,67 +4,109 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 import time
+import altair as alt
 
 # 제주도 중심 위도경도 변수 선언
 LAT = 33.38032
 LONG = 126.55
 
 # 데이터프레임 가져오기
+df_month = pd.read_csv("testdata/rank_by_month_type.csv", encoding='cp949')
 df = pd.read_csv("testdata\local_over_80.csv", encoding='cp949')
 
 def show_tab_trend(): 
+  #### 01. 월별 타입별 방문객 순위 ###
+  st.write("")
+  type_options_1 = df_month['MCT_TYPE'].unique()
+  # 컬럼 레이아웃 설정
+  col0_1, col0_2 = st.columns([3, 1])
+  # type 종류
+  type_options_1 = sorted(df_month['MCT_TYPE'].unique())
+
+  with col0_2:
+    # multiselect
+    selected_type_1 = st.multiselect('Select Type (최대 5개까지 선택 가능합니다)',
+                                     type_options_1,
+                                     default=[type_options_1[0]], # 기본값으로 하나 선택
+                                     key='graph1'
+                                     )
+    
+    # 선택한 MCT_TYPE이 5개를 넘으면 경고 메시지 표시
+    if len(selected_type_1) > 5:
+      st.warning("⚠️ 카테고리는 최대 5개까지 선택할 수 있습니다.")
+      selected_type_1 = selected_type_1[:5]  # max 5
+  
+  with col0_1:
+      if not selected_type_1:
+        selected_type_1 = [type_options_1[0]]  # 선택된 type이 없으면 기본값으로 첫 번째 타입 그래프 출력
+        
+      choosen_types = df_month[df_month['MCT_TYPE'].isin(selected_type_1)]
+
+      # 그래프 출력
+      chart = alt.Chart(choosen_types).mark_line(point=True).encode(
+        x=alt.X("MONTH:O", title="월"),
+        y=alt.Y('RANK_CNT:Q', title='평균 이용 건수 분위수'),
+        color='MCT_TYPE:N',                                    # 각 타입별 색상 구분
+        tooltip=['MONTH', 'MCT_TYPE', 'RANK_CNT']
+      ).properties(
+        title=f'\n선택된 {", ".join(selected_type_1)}의 월별 평균 이용 건수 분위수',
+      )
+      st.altair_chart(chart, use_container_width=True)
+
+
+  #### 02. 현지인 비중 상위 80% 식당 ###
   # 변수 초기화
-  if 'selected_ym' not in st.session_state:
-      st.session_state.selected_ym = None
-  if 'selected_type' not in st.session_state:
-      st.session_state.selected_type = None
+  if 'selected_ym_2' not in st.session_state:
+      st.session_state.selected_ym_2 = None
+  if 'selected_type_2' not in st.session_state:
+      st.session_state.selected_type_2 = None
   if 'random_seed' not in st.session_state:
       st.session_state.random_seed = int(time.time())
 
   col1, col2 = st.columns(2)
   with col1:
     # selectbox
-    ym_options = sorted(df['MONTH'].unique())
-    type_options = sorted(df['MCT_TYPE'].unique())
+    ym_options_2 = sorted(df['MONTH'].unique())
+    type_options_2 = sorted(df['MCT_TYPE'].unique())
 
     col1_1, col1_2 = st.columns(2)
     with col1_1:
-      selected_ym = st.selectbox('Select Month', ym_options)
+      selected_ym_2 = st.selectbox('Select Month', ym_options_2, key='map1')
     with col1_2:
-        selected_type = st.selectbox('Select Type', type_options)
+        selected_type_2 = st.selectbox('Select Type', type_options_2, key='map2')
     
     # 기본 아이콘 설정
     icon_name='utensils'
 
     # MONTH/TYPE이 변경된 경우에만 random_seed 갱신
-    if (selected_ym != st.session_state.selected_ym) or (selected_type != st.session_state.selected_type):
+    if (selected_ym_2 != st.session_state.selected_ym_2) or (selected_type_2 != st.session_state.selected_type_2):
       st.session_state.random_seed = int(time.time())
-      st.session_state.selected_ym = selected_ym
-      st.session_state.selected_type = selected_type
+      st.session_state.selected_ym_2 = selected_ym_2
+      st.session_state.selected_type_2 = selected_type_2
     
-    if st.session_state.selected_type == '커피':
+    if st.session_state.selected_type_2 == '커피':
       icon_name='coffee'
-    elif st.session_state.selected_type == '디저트/간식':
+    elif st.session_state.selected_type_2 == '디저트/간식':
         icon_name='ice-cream'
-    elif st.session_state.selected_type == '베이커리':
+    elif st.session_state.selected_type_2 == '베이커리':
       icon_name='bread-slice'
-    elif st.session_state.selected_type == '세계 요리':
+    elif st.session_state.selected_type_2 == '세계 요리':
       icon_name='globe'
-    elif st.session_state.selected_type == '맥주/요리주점':
+    elif st.session_state.selected_type_2 == '맥주/요리주점':
       icon_name='beer'
-    elif st.session_state.selected_type == '치킨':
+    elif st.session_state.selected_type_2 == '치킨':
       icon_name='drumstick-bite'
-    elif st.session_state.selected_type == '패스트푸드/간단한 식사':
+    elif st.session_state.selected_type_2 == '패스트푸드/간단한 식사':
       icon_name='hamburger'
-    elif st.session_state.selected_type == '피자':
+    elif st.session_state.selected_type_2 == '피자':
       icon_name='pizza-slice'
 
 
 
     # 선택한 MONTH과 TYPE_NAME에 따라 데이터 필터링
-    filtered_data = df[(df['MONTH'] == selected_ym) & (df['MCT_TYPE'] == selected_type)]
+    filtered_data = df[(df['MONTH'] == selected_ym_2) & (df['MCT_TYPE'] == selected_type_2)]
     # 현지인 비중 백분율로 변환
-    filtered_data['LOCAL_UE_CNT_RAT'] = filtered_data['LOCAL_UE_CNT_RAT'].astype(str) + '%'
+    filtered_data.loc[:, 'LOCAL_UE_CNT_RAT'] = filtered_data['LOCAL_UE_CNT_RAT'].astype(str) + '%'
 
     # 데이터는 최대 5개로 제한 & 랜덤으로 추출
     filtered_data = filtered_data.sample(n=min(5, len(filtered_data)), random_state=st.session_state.random_seed)
