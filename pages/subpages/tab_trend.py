5c764e9dc5b3c99955c7bb4bc0ef80cd9a393e96
kbr_1218 <kbr_uni@naver.com>
2024-10-11 12:09:37 +0900

    FEAT folium marker icon 디자인 수정

--- pages/subpages/tab_trend.py
+++ pages/subpages/tab_trend.py
@@ -33,12 +33,33 @@ def show_tab_trend():
     with col1_2:
         selected_type = st.selectbox('Select Type', type_options)
     
+    # 기본 아이콘 설정
+    icon_name='utensils'
 
     # MONTH/TYPE이 변경된 경우에만 random_seed 갱신
     if (selected_ym != st.session_state.selected_ym) or (selected_type != st.session_state.selected_type):
-       st.session_state.random_seed = int(time.time())
-       st.session_state.selected_ym = selected_ym
-       st.session_state.selected_type = selected_type
+      st.session_state.random_seed = int(time.time())
+      st.session_state.selected_ym = selected_ym
+      st.session_state.selected_type = selected_type
+    
+    if st.session_state.selected_type == '커피':
+      icon_name='coffee'
+    elif st.session_state.selected_type == '디저트/간식':
+        icon_name='ice-cream'
+    elif st.session_state.selected_type == '베이커리':
+      icon_name='bread-slice'
+    elif st.session_state.selected_type == '세계 요리':
+      icon_name='globe'
+    elif st.session_state.selected_type == '맥주/요리주점':
+      icon_name='beer'
+    elif st.session_state.selected_type == '치킨':
+      icon_name='drumstick-bite'
+    elif st.session_state.selected_type == '패스트푸드/간단한 식사':
+      icon_name='hamburger'
+    elif st.session_state.selected_type == '피자':
+      icon_name='pizza-slice'
+
+
 
     # 선택한 MONTH과 TYPE_NAME에 따라 데이터 필터링
     filtered_data = df[(df['MONTH'] == selected_ym) & (df['TYPE_NAME'] == selected_type)]
@@ -54,8 +75,10 @@ def show_tab_trend():
     # 필터링된 위치에 마커 추가
     for _, row in filtered_data.iterrows():
       folium.Marker(
+         # 중심 위치는 평균으로 설정
         location=[row['latitude'], row['longitude']],
-        popup=f"{row['MCT_NM']}"
+        popup=(f"{row['MCT_NM']}"),
+        icon=folium.Icon(color="orange", icon=icon_name, prefix='fa')
       ).add_to(m)
     
     # folium 지도를 streamlit에 표시
