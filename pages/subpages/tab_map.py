5e9ada462cf3ab8e56745144e4db54be27f5a74c
kbr1218 <kbr_uni@naver.com>
2024-10-16 01:20:10 +0900

    [STYLE] folium popup 메시지 설정

--- pages/subpages/tab_map.py
+++ pages/subpages/tab_map.py
@@ -3,8 +3,6 @@ import streamlit as st
 import folium
 from  streamlit_folium import folium_static
 
-import pandas as pd
-
 # 제주도 중심 위도경도 변수 선언
 LAT = 33.38032
 LONG = 126.55
@@ -15,12 +13,16 @@ def show_tab_map():
   with map_col1:
     m = folium.Map(location=[LAT, LONG], zoom_start=10)
 
+    # 지도 팝업창 출력 설정
+    # iframe = row['MCT_NM']+": <strong>"+row['LOCAL_UE_CNT_RAT']+"</strong>"
+    # popup = folium.Popup(iframe, min_width=100, max_width=300)
+
     # 필터링된 위치에 마커 추가
     # for _, row in filtered_data.iterrows():
     folium.Marker(
     # 중심 위치는 평균으로 설정
       location=[LAT, LONG],
-      popup=("제주도"),
+      popup=("제주도"), # popup (추후 변경 필요)
       icon=folium.Icon(color="red", icon='heart', prefix='fa')
     ).add_to(m)
       
